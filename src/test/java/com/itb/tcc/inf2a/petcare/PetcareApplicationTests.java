cd19ada99f796338a51f430bf3c11f7eab8025fc
yasmin barreto <yasminbarreto071@gmail.com>
2023-08-24 07:51:21 -0300

    criação do ambiente de desenvolvimento

---
+++ src/test/java/com/itb/tcc/inf2a/petcare/PetcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2a.petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
