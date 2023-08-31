c2a04dc95ae3058d243f5a325375160c13244912
yasmin barreto <yasminbarreto071@gmail.com>
2023-08-31 08:10:49 -0300

    criação da camada model

---
+++ src/main/java/com/itb/tcc/inf2a/petcare/model/Usuario.java
@@ -0,0 +1,128 @@
+package com.itb.tcc.inf2a.petcare.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
+
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String cpf;
+	private String nome;
+	private String telefone;
+	private String email;
+	private String senha;
+	private String logradouro;
+	private String cep;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	
+	private boolean codStatusUsuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public boolean isCodStatusUsuario() {
+		return codStatusUsuario;
+	}
+
+	public void setCodStatusUsuario(boolean codStatusUsuario) {
+		this.codStatusUsuario = codStatusUsuario;
+	}
+	
+	
+	
+}
